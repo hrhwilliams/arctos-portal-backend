31a2b0b2b6a0c8ecd137f610d189c017138fcf25
Harlan Williams <hrw@ku.edu>
2026-09-16 11:17:27 -0700

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+#[must_use]
+pub const fn one() -> i32 {
+    1
+}
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
     Ok(())
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn placeholder() {
+        assert!(one() == 1)
+    }
 }
